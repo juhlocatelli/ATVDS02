99be3e5c65be6968f3d10abd1905de7aa345afe1
Julia Locatelli <julocatelli66@gmail.com>
2025-05-26 23:00:34 -0300

    Adição dos comentários

--- Main.java
+++ Main.java
@@ -1,15 +1,15 @@
-import javax.swing.*;
+import javax.swing.*;//Biblioteca
 
 public class Main {
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> {
-            JFrame frame = new JFrame("Tela de Cadastro");
-            Cadastro cadastro = new Cadastro();
-            frame.setContentPane(cadastro.getPanel());
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            frame.pack();
-            frame.setLocationRelativeTo(null);
-            frame.setVisible(true);
+        SwingUtilities.invokeLater(() -> {//evita que a tela trave
+            JFrame frame = new JFrame("Tela de Cadastro");//criando a janela
+            Cadastro cadastro = new Cadastro();// criando um objeto Cadastro
+            frame.setContentPane(cadastro.getPanel());// conteudo da janela sendo retornado pelo método get
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//cuida da saída
+            frame.pack();//cuida do ajuste para o tamanho da tela
+            frame.setLocationRelativeTo(null);//responsável por centralizar no computador
+            frame.setVisible(true);// torna visivel
         });
     }
 }
